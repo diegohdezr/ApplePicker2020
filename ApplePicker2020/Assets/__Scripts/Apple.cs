4f64936146053650d549b93a49fed6085674c9ab
diegohdezr <diego.hdez@hotmail.com>
2021-08-02 19:52:17 -0500

    apple destroy
    destroy the apple when it gets too low, right now the value is -20 but it needs to be calibrated to the current screen

--- ApplePicker2020/Assets/__Scripts/Apple.cs
+++ ApplePicker2020/Assets/__Scripts/Apple.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    public static float bottomY = -20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Apple : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < bottomY) 
+        {
+            Destroy(this.gameObject);
+        }
         
     }
 }
