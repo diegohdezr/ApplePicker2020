12ba3158156edd9576ecb8d39f75c0c54444a614
diegohdezr <diego.hdez@hotmail.com>
2021-08-09 19:55:18 -0500

    baskets follow mouse
    use the input clas so that the baskets now follow the mouse

--- ApplePicker2020/Assets/__Scripts/ApplePicker.cs
+++ ApplePicker2020/Assets/__Scripts/ApplePicker.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
-
-
-
-
-
+    [Header("Set in inspector")]
+    public GameObject               basketPrefab;
+    public int                      numBaskets = 3;//numero de vidas o en este caso canastas
+    public float                    basketBottomY = -14;//posicion de la primer canasta
+    public float                    basketSpacingY = 2;//espacio entre canastas
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
     }
 
     // Update is called once per frame
--- ApplePicker2020/Assets/__Scripts/Basket.cs
+++ ApplePicker2020/Assets/__Scripts/Basket.cs
@@ -13,6 +13,19 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //get the current screen position from the input class
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //the camer's z position sets how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+
+        //conver the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //move the x position of this basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
     }
 }
